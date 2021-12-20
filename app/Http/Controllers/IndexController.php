a216f13310092a86806e85af298ce47e0fb1341f
charynepesov <nepesov.chary@ogr.ahievran.edu.tr>
2021-12-20 16:46:51 +0300

    Added home page design(Fetch posts, Search, etc)

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $posts = Post::all();
+        return view('index', ['posts'=>$posts]);
     }
 }
